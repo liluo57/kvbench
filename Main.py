6f72d6d1a41ea71197d0e96c4a832e1c3e8d18d6
lyh <lyh200757@outlook.com>
2026-09-13 17:14:11 +0800

    [chore]prepare for merge

--- Main.py
+++ Main.py
@@ -18,6 +18,7 @@ from methods import (
     FullPrefillTransformer,
     HypicMethod,
     NaiveTransformer,
+    DependencyAnalysisMethod,
 )
 from tasks import (
     AgentBenchFlowTask,
@@ -45,14 +46,29 @@ def Main() -> None:
 
     MAX_SAMPLES = 4
     tasks = [
-        KVCommMMLUTask(maxSamples=MAX_SAMPLES, agentCount=5),
-        KVCommGSM8KTask(maxSamples=MAX_SAMPLES, agentCount=3),
-        KVCommHumanEvalTask(maxSamples=MAX_SAMPLES, agentCount=5),
-        KVCommCopyTask(nCases=MAX_SAMPLES, agentCount=5),
+        NIAHShuffleTask(maxSamples=MAX_SAMPLES),
+        CWEShuffleTask(maxSamples=MAX_SAMPLES),
+        VTShuffleTask(maxSamples=MAX_SAMPLES),
+        MusiqueTask(maxSamples=MAX_SAMPLES),
+        SamsumTask(maxSamples=MAX_SAMPLES),
+        WikimQATask(maxSamples=MAX_SAMPLES),
+        # FreshGapTask(nCases=MAX_SAMPLES),
+        # GovReportTask(maxSamples=MAX_SAMPLES, nChunks=1, maxSampleLength=32768, tag="1"),
+        # GovReportTask(maxSamples=MAX_SAMPLES, nChunks=4, maxSampleLength=32768, tag="4"),
+        # GovReportTask(maxSamples=MAX_SAMPLES, nChunks=8, maxSampleLength=32768, tag="8"),
+        # GovReportTask(maxSamples=MAX_SAMPLES, nChunks=16, maxSampleLength=32768, tag="16"),
+        # HotpotQATask(maxSamples=MAX_SAMPLES),
+        # MultiNewsTask(maxSamples=MAX_SAMPLES),
+        # TriviaQATask(maxSamples=MAX_SAMPLES),
+        # KVCommMMLUTask(maxSamples=MAX_SAMPLES, agentCount=5),
+        # KVCommGSM8KTask(maxSamples=MAX_SAMPLES, agentCount=3),
+        # KVCommHumanEvalTask(maxSamples=MAX_SAMPLES, agentCount=5),
+        # KVCommCopyTask(nCases=MAX_SAMPLES, agentCount=5),
     ]
 
-    MAX_NEW_TOKENS = 512
+    MAX_NEW_TOKENS = 64
     methods = [
+        DependencyAnalysisMethod(gpuNums=1, perfWeight=1, maxNewTokens=MAX_NEW_TOKENS),
         # HypicMethod(
         #     maxNewTokens=MAX_NEW_TOKENS,
         #     gpuNums=2,
@@ -77,22 +93,22 @@ def Main() -> None:
         #     picMode="transition_rope",
         #     tag="transition_rope",
         # ),
-        HypicMethod(
-            maxNewTokens=MAX_NEW_TOKENS,
-            gpuNums=2,
-            maxModelLen=256000,
-            memFractionStatic=0.90,
-            picMode="transition_rope_recompute",
-            tag="transition_rope_recompute",
-        ),
-        HypicMethod(
-            maxNewTokens=MAX_NEW_TOKENS,
-            gpuNums=2,
-            maxModelLen=256000,
-            memFractionStatic=0.80,
-            fullPrefill=True,
-            tag="full_prefill",
-        ),
+        # HypicMethod(
+        #     maxNewTokens=MAX_NEW_TOKENS,
+        #     gpuNums=2,
+        #     maxModelLen=256000,
+        #     memFractionStatic=0.90,
+        #     picMode="transition_rope_recompute",
+        #     tag="transition_rope_recompute",
+        # ),
+        # HypicMethod(
+        #     maxNewTokens=MAX_NEW_TOKENS,
+        #     gpuNums=2,
+        #     maxModelLen=256000,
+        #     memFractionStatic=0.80,
+        #     fullPrefill=True,
+        #     tag="full_prefill",
+        # ),
         # FullPrefillVllm(
         #     gpuNums=1, perfWeight=2, maxNewTokens=40960,
         #     gpuMemoryUtilization=0.85,
